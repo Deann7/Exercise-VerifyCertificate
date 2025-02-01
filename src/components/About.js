df09b0b275b39a7dfdc4546a56f6837dd305a0ee
Deann7 <deandronas04@gmail.com>
2025-02-01 19:03:49 +0700

    change message

--- src/components/About.js
+++ src/components/About.js
@@ -6,9 +6,9 @@ export default function About() {
     >
       <div className="container mx-auto flex flex-col md:flex-row items-center gap-8 md:gap-16">
         <div className="w-full md:w-1/2 flex justify-center transform hover:scale-105 transition-all duration-300">
-          <img 
-            src="/Computer.png" 
-            alt="Certificate Example" 
+          <img
+            src="/ContohSertifikat.jpg"
+            alt="Certificate Example"
             className="max-w-full md:max-w-lg"
           />
         </div>
@@ -19,31 +19,32 @@ export default function About() {
           </h2>
           <div className="space-y-4">
             <p className="text-gray-700 text-base sm:text-lg">
-              Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-              Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-              when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+              Exercise Verify adalah platform online yang dirancang untuk
+              memverifikasi keaslian sertifikat yang diterbitkan oleh Exercise
+              FTUI. Layanan ini memastikan bahwa setiap sertifikat yang diterima
+              oleh peserta adalah asli dan valid.
             </p>
             <p className="text-gray-700 text-base sm:text-lg">
-              It has survived not only five centuries, but also the leap into electronic typesetting, 
-              remaining essentially unchanged. It was popularised in the 1960s with the release of 
-              Letraset sheets containing Lorem Ipsum passages.
+              Platform ini memudahkan proses verifikasi dengan sistem yang cepat
+              dan akurat. Dengan demikian, transparansi dan kepercayaan terhadap
+              sertifikat dapat terjaga dengan baik.
             </p>
           </div>
         </div>
       </div>
 
-      <img 
-        src="/ak2.png" 
+      <img
+        src="/ak2.png"
         alt="Decoration"
         className="absolute top-0 opacity-40 right-0 w-28 sm:w-40 md:w-80 h-auto animate-float z-10"
       />
-      <img 
-        src="/bk2.png" 
+      <img
+        src="/bk2.png"
         alt="Decoration"
         className="absolute bottom-0 opacity-40 right-0 w-12 sm:w-16 md:w-28 h-auto animate-float delay-150"
       />
-      <img 
-        src="/bl2.png" 
+      <img
+        src="/bl2.png"
         alt="Decoration"
         className="absolute bottom-0 opacity-40 left-0 w-12 sm:w-16 md:w-28 h-auto animate-float delay-300"
       />
